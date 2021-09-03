14e6d2c10bd127b1f0556f7dff47b62f0baff86e
Samuel <samlee1097@yahoo.com>
2021-09-03 12:26:55 -0400

    Last change

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header");
-console.log("Here's your header:", header);
+const header = document.querySelector("#header")
+console.log("Here's your header:", header)
+
 
 /***** Deliverable 2 *****/
 header.style.color = "red";
 
 /***** Deliverable 3 *****/
-console.log("PLAYERS array looks like this:", PLAYERS);
-
-// const playerInfo = document.createElement("div");
-// playerInfo.className = "player";
-
 const playerNum = [];
 PLAYERS.forEach((info) => playerNum.push(info.number));
 
@@ -23,17 +19,22 @@ PLAYERS.forEach((info) => playerNickname.push(info.nickname));
 const playerPhoto = [];
 PLAYERS.forEach((info) => playerPhoto.push(info.photo));
 
-console.log(playerNum);
-
-const whatever = PLAYERS.map(function () {
-  const playerInfo = document.createElement("div");
-  playerInfo.className = "player";
-  const keyname = document.createElement("h3");
-
-  keyname.textContent = "Hello";
-  playerInfo.append(keyname);
-
-  document.querySelector(".player-container").append(playerInfo);
-});
 
 /***** Deliverable 4 *****/
+
+
+PLAYERS.forEach(function(info){ 
+    //Creates div tag in DOM
+    const playerInfo = document.createElement("div");
+    playerInfo.className = "player";
+    //Creates h3 tag in DOM
+    const keyname = document.createElement("h3");
+    keyname.textContent = info.name;
+    //Store keyname as a child of playerInfo div
+    playerInfo.append(keyname);
+    //Store playerInfo as a child of .player-container in HTML
+    document.querySelector(".player-container").append(playerInfo);
+
+    const imgTag = document.createElement("img");
+    imgTag.src = player.photo;
+});
