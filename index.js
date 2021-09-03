91823c8f63a2188677007c8e629db96bef31785f
Nick <nick.c.ruzicka@gmail.com>
2021-09-03 12:18:54 -0400

    switch

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
-
+const header = document.querySelector("#header");
+console.log("Here's your header:", header);
 
 /***** Deliverable 2 *****/
 header.style.color = "red";
@@ -9,10 +8,32 @@ header.style.color = "red";
 /***** Deliverable 3 *****/
 console.log("PLAYERS array looks like this:", PLAYERS);
 
-const playerInfo = document.createElement("div");
-playerInfo.className = "player";
+// const playerInfo = document.createElement("div");
+// playerInfo.className = "player";
 
-const playerNum = []; 
-PLAYERS.forEach(info => playerNum.push(info.number));
+const playerNum = [];
+PLAYERS.forEach((info) => playerNum.push(info.number));
+
+const playerName = [];
+PLAYERS.forEach((info) => playerName.push(info.name));
+
+const playerNickname = [];
+PLAYERS.forEach((info) => playerNickname.push(info.nickname));
+
+const playerPhoto = [];
+PLAYERS.forEach((info) => playerPhoto.push(info.photo));
+
+console.log(playerNum);
+
+const whatever = PLAYERS.map(function () {
+  const playerInfo = document.createElement("div");
+  playerInfo.className = "player";
+  const keyname = document.createElement("h3");
+
+  keyname.textContent = "Hello";
+  playerInfo.append(keyname);
+
+  document.querySelector(".player-container").append(playerInfo);
+});
 
 /***** Deliverable 4 *****/
