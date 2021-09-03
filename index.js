c5f7b73b197278d1526d2411477cd31af62c2a16
Nick <nick.c.ruzicka@gmail.com>
2021-09-03 11:09:10 -0400

    switch

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
-
+const header = document.querySelector("#header");
+console.log("Here's your header:", header.textContent);
 
 /***** Deliverable 2 *****/
 
+// ## Deliverable 2
+
+// Now that you have access to the `h1#header` element, use Javascript to change the element's font color to red (of course).
+
+header.style.color = "red";
 
 /***** Deliverable 3 *****/
 // console.log("PLAYERS array looks like this:", PLAYERS)
-
 
 /***** Deliverable 4 *****/
